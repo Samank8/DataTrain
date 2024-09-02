768c7daeec638bd0304948b4aabdbc0e9a5aa54c
Saman Kazemi <samankazemi607@yahoo.com>
2024-09-02 17:23:40 +0330

    DisplayTeams function added

--- Program.cs
+++ Program.cs
@@ -30,47 +30,11 @@ namespace DatabaseConnectionTest
                             break;
 
                         case 2:
-                            Console.Write("enter your team name: ");
-                            String TeamName = Console.ReadLine();
+                            AddNewaTeams(connection);
 
-                            Console.Write("enter the number of your team players: ");
-                            int PlayerCount = int.Parse(Console.ReadLine());
+                            Console.WriteLine("This is new Teams table :");
 
-                            Console.Write("enter the stadium name: ");
-                            String StadiumName = Console.ReadLine();
-
-                            Console.Write("enter the stadium capacity: ");
-                            int StadiumCapacity = int.Parse(Console.ReadLine());
-
-                            Console.Write("enter your team caoch name: ");
-                            String CoachName = Console.ReadLine();
-
-                            Console.Write("enter your team league: ");
-                            String League = Console.ReadLine();
-
-                            Console.Write("enter the number of title which your team won: ");
-                            int TitleCount = int.Parse(Console.ReadLine());
-
-
-                            string insertSql = "INSERT INTO Teams (TeamName, PlayerCount, StadiumName, StadiumCapacity, CoachName, League, TitleCount) " +
-                                             "VALUES (@TeamName, @PlayerCount, @StadiumName, @StadiumCapacity, @CoachName, @League, @TitleCount)";
-
-
-                            using (SqlCommand insertCommand = new SqlCommand(insertSql, connection))
-                            {
-                                insertCommand.Parameters.AddWithValue("@TeamName", TeamName);
-                                insertCommand.Parameters.AddWithValue("@PlayerCount", PlayerCount);
-                                insertCommand.Parameters.AddWithValue("@StadiumName", StadiumName);
-                                insertCommand.Parameters.AddWithValue("@StadiumCapacity", StadiumCapacity);
-                                insertCommand.Parameters.AddWithValue("@CoachName", CoachName);
-                                insertCommand.Parameters.AddWithValue("@League", League);
-                                insertCommand.Parameters.AddWithValue("@TitleCount", TitleCount);
-
-                                Console.Write("new Team successfuly added");
-
-                                int rowsAffected = insertCommand.ExecuteNonQuery();
-                                Console.WriteLine($"{rowsAffected}");
-                            }
+                            DisplayTeams(connection);   
                             break;
 
                         case 3:
@@ -254,5 +218,51 @@ namespace DatabaseConnectionTest
                 }
             }
         }
+
+        static void AddNewaTeams(SqlConnection connection)
+        {
+            Console.Write("enter your team name: ");
+            String TeamName = Console.ReadLine();
+
+            Console.Write("enter the number of your team players: ");
+            int PlayerCount = int.Parse(Console.ReadLine());
+
+            Console.Write("enter the stadium name: ");
+            String StadiumName = Console.ReadLine();
+
+            Console.Write("enter the stadium capacity: ");
+            int StadiumCapacity = int.Parse(Console.ReadLine());
+
+            Console.Write("enter your team caoch name: ");
+            String CoachName = Console.ReadLine();
+
+            Console.Write("enter your team league: ");
+            String League = Console.ReadLine();
+
+            Console.Write("enter the number of title which your team won: ");
+            int TitleCount = int.Parse(Console.ReadLine());
+
+
+            string insertSql = "INSERT INTO Teams (TeamName, PlayerCount, StadiumName, StadiumCapacity, CoachName, League, TitleCount) " +
+                             "VALUES (@TeamName, @PlayerCount, @StadiumName, @StadiumCapacity, @CoachName, @League, @TitleCount)";
+
+
+            using (SqlCommand insertCommand = new SqlCommand(insertSql, connection))
+            {
+                insertCommand.Parameters.AddWithValue("@TeamName", TeamName);
+                insertCommand.Parameters.AddWithValue("@PlayerCount", PlayerCount);
+                insertCommand.Parameters.AddWithValue("@StadiumName", StadiumName);
+                insertCommand.Parameters.AddWithValue("@StadiumCapacity", StadiumCapacity);
+                insertCommand.Parameters.AddWithValue("@CoachName", CoachName);
+                insertCommand.Parameters.AddWithValue("@League", League);
+                insertCommand.Parameters.AddWithValue("@TitleCount", TitleCount);
+
+                Console.Write("new Team successfuly added");
+
+                int rowsAffected = insertCommand.ExecuteNonQuery();
+                Console.WriteLine($"{rowsAffected}");
+            }
+        }
+
     }
 }
